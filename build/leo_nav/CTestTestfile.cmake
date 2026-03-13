9593dd05b5c260cd89781437dca069b3ed5e072e
SARATH KUMAR S K <112240199+sarathkumar-sk@users.noreply.github.com>
2026-03-13 19:53:08 +0000

    Delete build directory

--- build/leo_nav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/team6/ros_ws/src/leo_navigation_tutorial
-# Build directory: /home/team6/ros_ws/build/leo_nav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
